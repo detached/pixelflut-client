09bf4fb4c15c24a48e9e95832e0d940d067e2c4f
Simon Weis <simon.weis@1und1.de>
2016-05-28 02:18:51 +0200

    Make it executable

--- src/main/java/de/w3is/pixelflut/client/PixelflutClient.java
+++ src/main/java/de/w3is/pixelflut/client/PixelflutClient.java
@@ -1,5 +1,7 @@
 package de.w3is.pixelflut.client;
 
+import java.util.stream.Stream;
+
 /**
  * @author <a href="mailto:simon.weis@1und1.de">Simon Weis</a>
  * @since 5/27/16.
@@ -7,16 +9,25 @@ package de.w3is.pixelflut.client;
 public class PixelflutClient {
 
     public static void main(String[] args) throws Exception {
-        String server = "94.45.232.57";
-        int port = 23421;
 
-        PixelPicture pixelPicture = new PixelPicture("/home/simon/Pictures/flower.jpg");
+        if (args.length != 5) {
+            System.out.println("Pixelflut-client $host $port $image $offX $offY");
+            return;
+        }
+
+        String server = args[0];
+        int port = Integer.parseInt(args[1]);
+
+        PixelPicture pixelPicture = new PixelPicture(args[2]);
+
+        int offX = Integer.parseInt(args[3]);
+        int offY = Integer.parseInt(args[4]);
 
         //PixelflutTCP pixelflutTCP = new PixelflutTCP(server, port);
         PixelflutUDP pixelflutUDP = new PixelflutUDP(server, port);
 
         //pixelPicture.scaleTo(pixelflutUDP.getSize());
 
-        pixelPicture.printTo(pixelflutUDP, 0, 0);
+        pixelPicture.printTo(pixelflutUDP, offX, offY);
     }
 }
